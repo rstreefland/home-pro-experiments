bfaa67ed81d589c050a561b0e9f2d9a174079a2d
Rhys Streefland <rhys@streefland.co.uk>
2024-06-02 18:52:36 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,6 +4,7 @@
 # we're passing on API host info
 env | grep _ >> /etc/environment
 
+nohup python3 /root/home-pro-experiments/carbon-intensity/run.py &
+
 # Start the ssh server
 /usr/sbin/sshd -D
-nohup python3 /root/home-pro-experiments/carbon-intensity/run.py &
